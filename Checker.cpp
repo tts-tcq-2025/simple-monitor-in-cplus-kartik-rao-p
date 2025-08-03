4dff2c86e4c984e02f606ea8c965737aed7a4594
kartik-rao-p <kartikraopatange@gmail.com>
2025-08-03 23:09:22 +0530

    Update Checker.cpp reduce cyclomatic complexity in checker src file

--- Checker.cpp
+++ Checker.cpp
@@ -2,18 +2,48 @@
 #include <iostream>
 using namespace std;
 
-bool batteryIsOk(float temperature, float soc, float chargeRate) {
+bool checkTemperature(float temperature) {
   if(temperature < 0 || temperature > 45) {
     cout << "Temperature out of range!\n";
     return false;
-  } else if(soc < 20 || soc > 80) {
+  }
+  return true;
+}
+
+bool checkStateOfCharge(float soc) {
+  if(soc < 20 || soc > 80) {
     cout << "State of Charge out of range!\n";
     return false;
-  } else if(chargeRate > 0.8) {
+  }
+  return true;
+}
+
+bool checkChargeRate(float chargeRate) {
+  if (chargeRate>0.8) {
     cout << "Charge Rate out of range!\n";
     return false;
   }
   return true;
+}
+
+bool batteryIsOk(float temperature, float soc, float chargeRate) {
+  bool resultTemperature=checkTemperature(temperature);
+  bool resultSoc=checkStateOfCharge(soc);
+  bool resultChargeRate=checkChargeRate(chargeRate);
+  bool result;
+  result = resultTemperature && resultSoc && resultChargeRate;
+  return result;
+  // if(temperature < 0 || temperature > 45) {
+  //   cout << "Temperature out of range!\n";
+  //   return false;
+  // } else if(soc < 20 || soc > 80) {
+  //   cout << "State of Charge out of range!\n";
+  //   return false;
+  // } else if(chargeRate > 0.8) {
+  //   cout << "Charge Rate out of range!\n";
+  //   return false;
+  // }
+  // return true;
 }
 
 int main() {
